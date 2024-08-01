f65e33fd3064fb268ac516ab969cbf72a7a5cde1
JEN023__ <165226466+Ganadalam@users.noreply.github.com>
2024-08-02 01:37:30 +0900

    Title: 다양하게 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240802/다양하게 출력/print-in-variety.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    cout << "Total days in Year" << endl << 365 << endl << "Circumference rate" << endl << "3.1415926535"; // 소수가 길어져 잘리므로 문자 처리.
+
+    return 0;
+}

56f6e35734290fde849abd0ed4db56975befe4ad
JEN023__ <165226466+Ganadalam@users.noreply.github.com>
2024-09-14 11:16:54 +0900

    Title: 체크 다이아몬드, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240914/체크 다이아몬드/check-diamond.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 3; // 다이아몬드의 중간 부분 별의 수
+
+    // 위쪽 부분
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n - i; j++) {
+            cout << " ";  // 공백 출력
+        }
+        for (int j = 1; j <= i; j++) {
+            cout << "* "; // 별 출력
+        }
+        cout << endl;
+    }
+
+    // 아래쪽 부분
+    for (int i = n - 1; i >= 1; i--) {
+        for (int j = 1; j <= n - i; j++) {
+            cout << " ";  // 공백 출력
+        }
+        for (int j = 1; j <= i; j++) {
+            cout << "* "; // 별 출력
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
